d6a8a5b74e37ea2f1df2cbefcd05c85ea215c526
Jaycen <jaycen9887@gmail.com>
2017-08-24 22:34:12 -0400

    changing mysql connection to cleardb

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    port: "3306",
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burgers_db"
+    host: "us-cdbr-iron-east-05.cleardb.net",
+    user: "bb7caa8cec3cbf",
+    password: "39728c71",
+    database: "heroku_32700f8a4e1eb01"
 });
 
 connection.connect(function(err){
@@ -15,3 +14,18 @@ connection.connect(function(err){
 });
 
 module.exports = connection;
+
+/*mysql.createConnection({
+    port: "3306",
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+});*/
+
+
+/*  port: "5432",
+    host: "ec2-54-163-237-25.compute-1.amazonaws.com",
+    user: "hwycyfeedyopth",
+    password: "dcb4c8c33f7795e1c0bf82aa7a57cd4a03fd60e8de2d7baae666d83e928bbe51",
+    database: "d8au0ur2r86rav"*/
